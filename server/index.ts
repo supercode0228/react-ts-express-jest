fd9a249894fc3bb2ebc469a947b4943370670719
supercode0228 <bee0228angel@outlook.com>
2021-05-14 11:15:37 -0400

    define the router

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import routes from './routes';
 
 const app: Express = express();
 
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+app.use('/api/v1', routes);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
